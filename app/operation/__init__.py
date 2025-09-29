ecb76623529feb5e7f30f0584b5b03f3836b7d0e
edassasmz <edassasmz@gmail.com>
2025-09-29 19:10:24 -0400

    added app

---
+++ app/operation/__init__.py
@@ -0,0 +1,26 @@
+# Author: Edanur Sasmaz
+#Date: Sept 29,2025
+# operations.py
+
+class Operation:
+    @staticmethod
+    def addition(a: float, b:float) -> float:
+        return a + b
+    
+    @staticmethod
+    def subtraction(a: float, b:float) -> float:
+        return a - b
+    
+    @staticmethod
+    def multiplication(a: float, b: float) -> float:
+        return a * b
+    
+    @staticmethod
+    def division(a: float, b: float) -> float:
+        if b== 0:
+            raise ValueError("Division by zero is not allowed")
+        return a / b
+    
+    @staticmethod
+    def power(a: float, b: float) -> float:
+        return a ** b
